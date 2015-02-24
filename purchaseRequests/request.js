bd777691d51270e5ca81b6cbafd6b26d5dfcd726
Joseph Warner <hardolaf@hardolaf.com>
2015-02-24 13:15:31 -0500

    Changes

--- purchaseRequests/request.js
+++ purchaseRequests/request.js
@@ -9,7 +9,7 @@ function uploadFiles(form) {
   try {
 
     /* Name of the Drive folder where the files should be saved */
-    var dropbox = "Hackathon Dropbox";
+    var dropbox = "Event Planning/Hackathon/Purchase Requests";
     var folder, folders = DriveApp.getFoldersByName(dropbox);
 
     /* Find the folder, create if the folder does not exist */
@@ -22,12 +22,33 @@ function uploadFiles(form) {
     /* Get the file uploaded though the form as a blob */
     var blob = form.myFile;
     var file = folder.createFile(blob);
+    var sheet = SpreadsheetApp.openById('1j34J2Uv8gvx27I7DVvSvu7KzhZUF9LsGkq2c3mLrr1U');
+
+    /* Input data into spreadsheet */
+    var data = [form.requestorName,
+                form.requestorEmail,
+                form.task,
+                form.budgetNameExisting,
+                form.purpose,
+                form.desireDate,
+                form.requireDate,
+                form.total,
+                file.getName()];
+    sheet.appendRow(data);
 
     /* Set the file description as the name of the uploader */
     file.setDescription("Uploaded by " + form.requestorName);
 
-    /* Return the download URL of the file once its on Google Drive */
-    return "File uploaded successfully " + file.getUrl();
+    /* Email a link to the Doc as well as a PDF copy. */
+    MailApp.sendEmail({
+      to: "electronicsosu@gmail.com",
+      subject: 'ACTION: Purchase Request: ' + file.getName() + ' has been filed',
+      body: 'The following purchase request has been filed: ' + file.getUrl(),
+      attachments: file.getAs(MimeType.PDF),
+    });
+
+    /* The request has been filed successfully */
+    return "Request uploaded successfully.";
 
   } catch (error) {
 
